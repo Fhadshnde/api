b4d2f2bdeec022b498d226c54fac47bb5261c1f1
Fhadshnde <Fhadshnde32@gmail.com>
2024-12-31 04:13:45 +0300

    Your commit message

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 // تحديث المستخدم
 router.put("/:id", async (req, res) => {
