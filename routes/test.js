4361c88a1a725d6a1ba4dbbe1cebc4f9f0a77f80
Fhadshnde <Fhadshnde32@gmail.com>
2025-03-13 02:21:15 +0300

    edit

--- routes/test.js
+++ routes/test.js
@@ -8,7 +8,7 @@ router.get('/test', async (req, res) => {
             res.status(200).send('Test route is working.');
         }, 2000); // إعداد مهلة زمنية قصيرة لتجنب انتهاء المهلة الزمنية
     } catch (error) {
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).json({ message: 'Internal server error' })
     }
 });
 
