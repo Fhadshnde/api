ff71d6bd92c6f67857a120d995e1067b7b0b61d6
Fhadshnde <Fhadshnde32@gmail.com>
2025-03-13 02:02:32 +0300

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const router = require('./routes');
+const testRoute = require('./routes/test'); // إضافة هذا السطر
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", router);
+app.use("/api", testRoute); // إضافة هذا السطر
 
 const PORT = process.env.PORT || 8080;
 
---
+++ routes/test.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/test', async (req, res) => {
+    try {
+        // محاكاة عملية طويلة الأمد
+        setTimeout(() => {
+            res.status(200).send('Test route is working.');
+        }, 2000); // إعداد مهلة زمنية قصيرة لتجنب انتهاء المهلة الزمنية
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+module.exports = router;
