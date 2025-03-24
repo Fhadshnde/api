8979d0e26125afe9a526c52732aa0492af79e801
Fhadshnde <Fhadshnde32@gmail.com>
2025-03-24 03:23:01 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,25 +10,34 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+// إعداد CORS للسماح بالوصول من الجهة الأمامية
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
+
+// إعداد Body Parser
 app.use(bodyParser.json({ limit: '500mb' }));
 app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 500000, limit: '500mb' }));
+
+// إعداد Body Parsing و Cookies
 app.use(express.json());
 app.use(cookieParser());
 
+// إضافة المسارات
 app.use("/api", router);
-app.use("/api", testRoute);  // إضافة السطر لتشغيل المسار
+app.use("/api", testRoute);  // إضافة المسار الذي تم تعريفه في routes/test.js
 
 const PORT = process.env.PORT || 8080;
 
+// الاتصال بقاعدة البيانات ثم بدء الخادم
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Connected to DB");
         console.log("Server is running on port " + PORT);
     });
+}).catch((error) => {
+    console.log('Error connecting to database:', error);
 });
 
 module.exports = app;
